3b6b9665fc0607734ccb303b42cab09f9c46c265
Samiya Sadiq <samiyasadiq2002@gmail.com>
2023-12-19 21:01:51 +0530

    Update CaesarCipher.java

--- CaesarCipher.java
+++ CaesarCipher.java
@@ -1,32 +1,44 @@
-public class CaesarCipher {
-    public static String encrypt(String plainText, int shift) {
-        StringBuilder cipherText = new StringBuilder();
-
-        for (char ch : plainText.toCharArray()) {
-            if (Character.isLetter(ch)) {
-                char base = Character.isLowerCase(ch) ? 'a' : 'A';
-                cipherText.append((char) ((ch - base + shift) % 26 + base));
-            } else {
-                cipherText.append(ch);
-            }
+import java.util.*;
+ 
+public class CaesarCipher{
+    public static String encrypt(String plainText, int K){
+        plainText = plainText.toLowerCase();
+        String cipherText = "";
+        for (int i = 0; i < plainText.length(); i++){
+            int P = plainText.charAt(i) - 'a';
+            int keyVal = (P+K) % 26;
+            char replaceVal = (char)('a' + keyVal);
+            cipherText += replaceVal;
         }
-
-        return cipherText.toString();
+        return cipherText;
     }
-
-    public static String decrypt(String cipherText, int shift) {
-        return encrypt(cipherText, 26 - shift);
+ 
+    public static String decrypt(String cipherText, int K){
+        cipherText = cipherText.toLowerCase();
+        String plainText = "";
+        for (int i = 0; i < cipherText.length(); i++){
+            int P = cipherText.charAt(i) - 'a';
+            int keyVal = (P - K) % 26;
+            if (keyVal < 0){
+                keyVal = 26 + keyVal;
+            }
+            char replaceVal = (char)('a' + keyVal);
+            plainText += replaceVal;
+        }
+        return plainText;
     }
+ 
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the String for Encryption: ");
+        String message = sc.next();
+        System.out.println("Enter K Value: ");
+        int k = sc.nextInt();
 
-    public static void main(String[] args) {
-        String plaintext = "Hello, World!";
-        int shift = 3;
-
-        String encryptedText = encrypt(plaintext, shift);
-        String decryptedText = decrypt(encryptedText, shift);
-
-        System.out.println("Original Text: " + plaintext);
-        System.out.println("Encrypted Text: " + encryptedText);
-        System.out.println("Decrypted Text: " + decryptedText);
+        String encrypted = encrypt(message, k);
+        String decrypted = decrypt(encrypted, k);
+        System.out.println("The Encrypted Message is: "+ encrypted);
+        System.out.println("The Decrypted Message is: " + decrypted);
+        sc.close();
     }
 }
